64aee11f0b1f2e54e24fad2cbe125af326d6906e
Amelie1253 <adinh2@lion.lmu.edu>
2025-05-23 00:25:56 -0500

    implemented auth

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,21 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
-import LogIn from './LogIn.jsx'
-import About from './About.jsx'
-import SearchBar from './SearchBar.jsx'
-import Results from './Results.jsx'
-import Favorites from './Favorites.jsx'
+import LogIn from "./LogIn.jsx";
+import About from "./About.jsx";
+import SearchBar from "./SearchBar.jsx";
+import Results from "./Results.jsx";
+import Favorites from "./Favorites.jsx";
+import { supabase } from "./supabase";
 
 function App() {
-  const [user, setUser] = useState(false)
-  const [results, setResults] = useState(false)
-  const [isLogin, setIsLogin] = useState(false)
-  const [isHome, setIsHome] = useState(false)
-  const [isFavorites, setIsFavorites] = useState(false)
-  const [isAbout, setIsAbout] = useState(false)
+  const [user, setUser] = useState(false);
+  const [results, setResults] = useState(false);
+  const [isLogin, setIsLogin] = useState(false);
+  const [isHome, setIsHome] = useState(false);
+  const [isFavorites, setIsFavorites] = useState(false);
+  const [isAbout, setIsAbout] = useState(false);
+  const [isSignUp, setIsSignUp] = useState(true);
 
   const changePage = (page) => {
     setIsLogin(false);
@@ -34,25 +36,32 @@ function App() {
 
   return (
     <>
-    <header>
-      <h1>STEM-Spire</h1>
-      <nav idName="navBar">
-        <button onClick={() => changePage("login")}>Log In</button>
-        <button onClick={() => changePage("home")}>Home</button>
-        <button onClick={() => changePage("favorites")}>Favorites</button>
-        <button onClick={() => changePage("about")}>About</button>
-      </nav>
-    </header>
-    {isLogin && <LogIn></LogIn>}
-    {isAbout && <About></About>}
-    {isHome && <div>
-      <h2>Search</h2>
-      <SearchBar></SearchBar>
-      {results && <Results></Results>}
-    </div>}
-    {isFavorites && <Favorites></Favorites>}
+      <header>
+        <h1>STEM-Spire</h1>
+        <nav idName="navBar">
+          <button onClick={() => changePage("login")}>Log In</button>
+          <button onClick={() => changePage("home")}>Home</button>
+          <button onClick={() => changePage("favorites")}>Favorites</button>
+          <button onClick={() => changePage("about")}>About</button>
+        </nav>
+      </header>
+      {isLogin && (
+        <LogIn
+          isSignUp = {isSignUp}
+          setIsSignUp={setIsSignUp}
+        />
+      )}
+      {isAbout && <About />}
+      {isHome && (
+        <div>
+          <h2>Search</h2>
+          <SearchBar />
+          {results && <Results />}
+        </div>
+      )}
+      {isFavorites && <Favorites />}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/LogIn.jsx
+++ src/LogIn.jsx
@@ -1,8 +1,44 @@
-export default function LogIn() {
-    return (
-        <>
-        <h2>Log In</h2>
-        <p>Log In/Account creation form goes here!</p>
-        </>
-    )
+import { supabase } from './supabase'; // update this path
+
+export default function LogIn({isSignUp, setIsSignUp}) {
+
+  async function signUp(e) {
+    e.preventDefault();
+    setIsSignUp(true);
+    const email= document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+    console.log('Email:', email);
+    console.log('Password:', password);
+    if (isSignUp) {
+        const { error } = await supabase.auth.signUp({ email, password })
+        // if (error) setMessage(error.message)
+        // else setMessage('Check your email to confirm sign up.')
+      } else {
+        const { error } = await supabase.auth.signInWithPassword({ email, password })
+        // if (error) setMessage(error.message)
+        // else setMessage('Logged in!')
+      }
+
+    }
+
+  return (
+    <>
+      <h2>Log In</h2>
+      <div>
+        <form>
+          <p>Sign Up!</p>
+          <label>Email</label>
+          <input id="email" placeholder="Email"></input>
+          <label>Password</label>
+          <input
+            id="password"
+            placeholder="*************"
+          ></input>
+          <button onClick={signUp}>Sign Up</button>
+          <p>If you already have an account,</p>
+          <button>Log In</button>
+        </form>
+      </div>
+    </>
+  );
 }
