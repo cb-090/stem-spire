0ccae162388e39ba65211e9e617137eace7681f1
Tara Ramanathan <tramanat@lion.lmu.edu>
2025-06-14 01:31:22 -0700

    style about page

--- src/About.jsx
+++ src/About.jsx
@@ -1,15 +1,18 @@
-import './About.css'; // Make sure to create this CSS file
+import './About.css';
+import "./App.css";
 
 export default function About() {
   return (
-    <div className="about-page">
-      <h2 className="section-title">About STEM-Spire</h2>
+    <div className="page">
+      <div className='page-header' >
+        <h2>About STEM-Spire</h2>
+      </div>
       <p className="mission-blurb"> The underrepresentation of women and gender-nonconforming individuals in STEM fields 
       remains a significant barrier to inclusivity, often due to a lack of accessible resources, mentorship, and 
       encouragement. Our website aims to collect, categorizes,and curates STEM-related resources for high school and middle school students who may not see themselves represented 
       in the field.</p>
 
-      <h2 className="section-title">About the Team</h2>
+      <h2 className="section-title">About The Team</h2>
       <div className="team-grid">
         <div className="team-member">
           <h3>Amelie</h3>
